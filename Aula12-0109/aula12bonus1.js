6eb076ed2390ec9bf224d9f3990779cc2a0f9955
fabio-as <65870289+fabio-as@users.noreply.github.com>
2021-09-08 20:37:12 -0300

    Aula12-0109

---
+++ Aula12-0109/aula12bonus1.js
@@ -0,0 +1,10 @@
+/* digitalHouse()
+Crie a função digitalHouse() que receberá 2 números como parâmetros. 
+A função deve imprimir na tela os números de 1 a 100, mas levando em conta os seguintes critérios:
+Se o número a ser impresso for um múltiplo do primeiro parâmetro inserido, 
+você deve exibir a string "Digital" em vez do número.
+Se o número a ser impresso for um múltiplo do segundo parâmetro inserido, 
+você deve exibir a string "House" em vez disso.
+Se o número a ser imprimir for um múltiplo de ambos os parâmetros, 
+você deve exibir a string "Digital House" em vez do número.
+*/
---
+++ Aula12-0109/aula12bonus2.js
@@ -0,0 +1,16 @@
+/* somaArray()... Reloaded
+Vamos pegar o exercício sumArray() que fizemos na classe arrays, mas desta vez 
+vamos modificá-lo para que ele possa receber um array de números de qualquer quantidade de elementos.
+Se você não se lembrar da instrução original:
+Você deve criar uma função somaArray que aceita uma matriz de números e retorna a soma de todos eles. 
+    exemplo: 
+    somaArray([1,2,3]) // 6
+    somaArray([10,  3,  10,  4]) // 27
+    somaArray([-5,100]) // 95
+e como estamos ajustando as funções, vamos fazer o mesmo com a função do exercício de simulação join(). 
+você deve modificar a função join()  para que ela possa receber uma matriz de strings de qualquer quantidade de elementos. 
+Importante: Você não pode usar o método  original Array.join().
+por exemplo:
+join(["o","l","a"]) deve retornar a string "ola".
+join(["t", "c","h","a,"u"]) deve retornar a string "tchau". 
+*/
